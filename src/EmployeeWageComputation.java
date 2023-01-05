87e190cf68b4c112d8c5a560782b7d99b5155075
Ashuhauhan <ashuc306@gmail.com>
2023-01-06 00:36:00 +0530

    switch appoarch for previous cases

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,24 +1,39 @@
+import java.util.Scanner;
 import java.util.Random;
 public class EmployeeWageComputation {
         public static void main(String[] args){
 
-                    int r;
-                    Random ran = new Random();
-                    r = ran.nextInt(2);
-                    System.out.println("random numbers are   " + r);
-                    if (r == 0) {
-                        System.out.println("Employee is absent");
-                    }
-                    if (r == 1) {
-                        System.out.println("Employee is present");
+            int wageperhour = 20;
+            int fulltimehours = 8;
+            int parttimehours = 4;
+            int r;
+            Random ran = new Random();
+            r = ran.nextInt(2);
+            System.out.println("random numbers are   " + r);
+            if (r == 0) {
+                System.out.println("Employee is absent");
+            }
+            if (r == 1) {
+                System.out.println("Employee is present");
 
-                        int wageperhour = 20;
-                        int parttimehours = 4;
-                        int partimewage;
-                        partimewage = wageperhour * parttimehours ;
-                        System.out.println("Daily wage of partime working Employee is " + partimewage);
-                    }
-
+                Scanner myinput = new Scanner(System.in);
+            System.out.println("Enter 1 for fulltimewage else Enter 2 for partimewage calculation");
+            int data = myinput.nextInt();
+            switch(data){
+                case 1:
+                    int dailywage;
+                    dailywage = wageperhour * fulltimehours;
+                    System.out.println("daily wage of fulltime working Employee is " + dailywage);
+                    break;
+                case 2:
+                    int partimewage;
+                    partimewage= wageperhour * parttimehours;
+                    System.out.println("Daily wage of partime working Employee is " + partimewage);
+                    break;
+                default:
+                    System.out.println("hi");
+                }
+            }
 
         }
 
