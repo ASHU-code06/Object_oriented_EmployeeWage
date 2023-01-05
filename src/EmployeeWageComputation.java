bcb4d27f38dcc432c28e12bee333fcdcf92d7098
Ashuhauhan <ashuc306@gmail.com>
2023-01-05 23:55:56 +0530

    part time wage of employee

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,17 +1,25 @@
 import java.util.Random;
 public class EmployeeWageComputation {
         public static void main(String[] args){
-            int r;
-            Random ran = new Random();
-            r= ran.nextInt(2);
-            System.out.println("random numbers are   "   +r);
-            if(r==0){
-                System.out.println("Employee is absent");
-            }
 
-            if(r==1){
-                System.out.println("Employee is present");
-            }
+                    int r;
+                    Random ran = new Random();
+                    r = ran.nextInt(2);
+                    System.out.println("random numbers are   " + r);
+                    if (r == 0) {
+                        System.out.println("Employee is absent");
+                    }
+                    if (r == 1) {
+                        System.out.println("Employee is present");
+
+                        int wageperhour = 20;
+                        int parttimehours = 4;
+                        int partimewage;
+                        partimewage = wageperhour * parttimehours ;
+                        System.out.println("Daily wage of partime working Employee is " + partimewage);
+                    }
+
+
         }
-    }
+
 }
