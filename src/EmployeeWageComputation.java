8a1b61a245eb5bbc9fbf674032224c331113328b
Ashuhauhan <ashuc306@gmail.com>
2023-01-08 18:38:13 +0600

    Calculation of Monthly Employee Wage

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -3,10 +3,12 @@ import java.util.Random;
 public class EmployeeWageComputation {
 
     public static void main(String[] args){
-        System.out.println("Welcome");
-            MainEmp obj = new MainEmp();
-            obj.monthlywageofemployee();
 
+        System.out.println("Welcome");
+
+            MainEmp obj = new MainEmp(4,8);
+            int totalMonthlyWage = obj.getMonthlyWageOfEmployee(20,20);
+            System.out.println("the monthly wage of employee is "+totalMonthlyWage+"/-  Rs");
         }
     }
 
--- src/MainEmp.java
+++ src/MainEmp.java
@@ -1,53 +1,56 @@
 import java.util.Random;
 
 public class MainEmp {
-    public static void monthlywageofemployee() {
-        int monthlyhours = 0;
-        int check;
-        int totalworkinghours = 0;
-        int add = 0;
-        System.out.println("Monthly attendence of employee");
-        while (monthlyhours < 100) {
-            Random r = new Random();
-            check = r.nextInt(3);
-            if (check == 0) {
-                System.out.println("Employee is absent");
-            } else if (check == 1) {
-                System.out.println("Employee is working fulltime = 8 hrs ");
-                totalworkinghours = 8;
-                add = add + totalworkinghours;
-            } else if (check == 2) {
-                System.out.println("Employee is working parttime = 4 hrs ");
-                totalworkinghours = 4;
-                add = add + totalworkinghours;
-            }
+    private int partTimeHrs;
+    private int fullTimeHrs;
+    public MainEmp(int _partTimeHrs,int _fullTimeHrs) {
+        this.partTimeHrs=_partTimeHrs;
+        this.fullTimeHrs=_fullTimeHrs;
+    }
 
-            monthlyhours++;
-            System.out.println(" total hours the employee work is  " + add + "hrs");
+    public int getMonthlyWageOfEmployee(int wagePerHour,int monthlyWorkingDays) {
+
+        boolean isFullTime = isEmployeeFullTime();
+        int workingHours = isFullTime?fullTimeHrs:partTimeHrs;
+        int totalHours = 0;
+
+        for (int i =0;i<monthlyWorkingDays;i++){
+            boolean isEmployeePresent = isEmployeePresent();
+           totalHours=totalHours+(isEmployeePresent?workingHours:0);
+
         }
-        MainEmp obj = new MainEmp();
-        obj.calculation("BRIDGELABZ",100,add);
-        obj.calculation("INDIAN GLYCOLS ",30,add);
-        obj.calculation("YARA FERTILIZERS",90,add);
-        obj.calculation("MAHINDRA",63,add);
-        obj.calculation("TYSON",80,add);
-        obj.calculation("LG",40,add);
-        obj.calculation("TATA",70,add);
 
+        int totalMonthlyWages = totalHours*wagePerHour;
+        return totalMonthlyWages;
+
+    }
+    private boolean isEmployeePresent(){
+        int check;
+        Random r = new Random();
+        check = r.nextInt(2);
+        if (check == 0) {
+            System.out.println("Employee is absent");
+            return false;
+        } else  {
+            System.out.println("Employee is present");
+            return true;
+        }
+        /*can do like this too ; return check==0?false:true;
+         if condition :- 1 = present & 0 = absent */
+    }
+    private boolean isEmployeeFullTime(){
+        int check;
+        Random r = new Random();
+        check = r.nextInt(2);
+        if (check == 0) {
+            System.out.println("Employee is parttime");
+            return false;
+        } else  {
+            System.out.println("Employee is fulltime");
+            return true;
+        }
 
     }
 
-    public void calculation(String comp, int wageperhour, int monthlyworkinghours) {
-        int monthlywageofEmployee = wageperhour*monthlyworkinghours;
-        System.out.println("\n");
-        System.out.println("----------------------------------------------------------------------------------");
-        System.out.println("\n");
-        System.out.println("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM FOR "+comp+" COMPANY");
-        System.out.println("The wageperhour of company is "+wageperhour+"/-  Rs");
-        System.out.println("Maximum working hrs in company for employee is 100hrs");
-        System.out.println("Total working hrs of employee is "+monthlyworkinghours+" hrs");
-        System.out.println("Monthly wage is "+monthlywageofEmployee+"/-  Rs");
-
-    }
 }
 
